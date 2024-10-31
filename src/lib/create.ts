21ed15d6cabfce4594117b0daf3348929672fd04
Ted <me@t3d.uk>
2024-10-31 15:31:38 +0000

    feat: ✨ rate limitng (ss)

--- src/lib/create.ts
+++ src/lib/create.ts
@@ -1,4 +1,5 @@
 import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { headers } from "next/headers";
 
 function generateKey(length: number) {
   const characters =
@@ -15,8 +16,20 @@ function generateKey(length: number) {
 
 export async function create(link: string, length: number = 5) {
   const cloudflareContext: any = await getCloudflareContext();
+  const ip = (await headers()).get("cf-connecting-ip") || "no ip";
 
   const linkKeyValueStore: KVNamespace = cloudflareContext.env.LINKS;
+  const limitKeyValueStore: KVNamespace = cloudflareContext.env.LIMITS;
+
+  const limit = await limitKeyValueStore.get(ip);
+
+  if (limit != null) {
+    if (parseInt(limit) > Date.now() - 5000) {
+      return {
+        error: "Rate limit exceeded",
+      };
+    }
+  }
 
   const key = generateKey(length);
 
@@ -26,7 +39,20 @@ export async function create(link: string, length: number = 5) {
     return create(link, length + 1);
   }
 
-  await linkKeyValueStore.put(key, link);
+  try {
+    const res = await linkKeyValueStore.put(key, link);
 
-  return key;
+    await limitKeyValueStore.put(ip, Date.now().toString(), {
+      expirationTtl: 5000,
+    });
+  } catch (error) {
+    console.error(error);
+    return {
+      error: "Failed to create link",
+    };
+  }
+
+  return {
+    key: key,
+  };
 }
